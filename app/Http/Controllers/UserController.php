8c891c0d6b1df49d6ad09984d332bf8a7d344713
lekphin <phinlek20@gamil.com>
2023-05-20 16:23:58 +0700

    create show user and farm resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -14,6 +15,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
+        $users = UserResource::collection($users);
         return response()->json(['success'=>true, 'data'=>$users],200);
     }
 
