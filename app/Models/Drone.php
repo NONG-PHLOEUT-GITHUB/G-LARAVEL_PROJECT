95bbae204e10950afd5f0c43223873bcd6023e6e
ileanan <dynahen133@gmail.com>
2023-05-20 15:28:01 +0700

    update on drone

--- app/Models/Drone.php
+++ app/Models/Drone.php
@@ -8,4 +8,43 @@ use Illuminate\Database\Eloquent\Model;
 class Drone extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'drone_type',
+        'drone_name',
+        'battery',
+        'playload_capacity',
+        'user_id',
+        'plan_id',
+    ];
+
+    // FUNTION TO CREATE AND UPDATE drone
+    public static function store($request, $id=null){
+        $drone = $request->only(["drone_type","drone_name","battery","playload_capacity","user_id","plan_id"]);
+        $drone = self::updateOrCreate(["id" => $id], $drone);
+        return $drone;
+    }
+    //==================RELATION==============================================================
+    // Relation to user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relation to many map
+    public function map()
+    {
+        return $this->belongsTo(Map::class);
+    }
+
+    // Relation to plan
+     public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    // Relation to many location
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
 }
